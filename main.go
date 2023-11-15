83f2ca1ea97898997baea0680e968b083984f706
xenophobi <116875873+ItzPrism@users.noreply.github.com>
2023-11-15 13:03:03 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+import (
+"fmt"  
+"net"
+"log"
+)
+func getPortAndIP() {
+var serverIP string
+var port int
+fmt.Println("Input server IP")
+  fmt.Scan(serverIP)
+  fmt.Println("Input Port)
+    fmt.Scan(port)
+return port
+return serverIP
+}
+func main() {
+  getPortAndIP()
+  fmt.Println(serverIP + ":" + port)
+}
